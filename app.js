9bb081100c419f88af6d064914af8c61ad69b6e7
David McIntyre <davidrobert.mcintyre@gmail.com>
2023-05-17 18:13:59 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,19 @@
-const pianoKeys = document.querySelectorAll('.key');
+const pianoKeys = document.querySelectorAll('.key')
 
 function playSound(newUrl) {
-  console.log(newUrl);
-  const audio = new Audio(newUrl);
-  audio.play();
-
-  // Change opacity of clicked key
-  this.style.opacity = '0.9';
-
-  // Revert opacity back to 1 after 0.5 seconds
-  setTimeout(() => {
-    this.style.opacity = '1';
-  }, 500);
+    console.log(newUrl)
+    new Audio(newUrl).play()
 }
 
+
 pianoKeys.forEach((pianoKey, i) => {
-  const number = i < 9 ? '0' + (i + 1) : (i + 1);
-  const keys = ['02', '04', '06', '09', '11', '14', '16', '18', '21', '23', '01', '03', '05', '07', '08', '10', '12', '13', '15', '17', '19', '20', '22', '24'];
-  const newUrl = '24-piano-keys/key' + keys[i] + '.mp3';
-  pianoKey.addEventListener('click', playSound.bind(pianoKey, newUrl));
-});
+    const number = i < 9 ? '0' + (i + 1) : (i + 1)
+    const keys =['02','04','06',      '09','11',     '14','16','18',       '21','23',
+		     '01','03','05','07','08','10','12','13','15','17','19','20','22','24']
+const newUrl = '24-piano-keys/key'+ keys[i] + '.mp3'
+    pianoKey.addEventListener('click', () => playSound(newUrl))
+})
 
 var width = window.innerWidth;
-var height = window.innerHeight;
-console.log('Width: ' + width + ', Height: ' + height);
+    var height = window.innerHeight;
+    console.log('Width: ' + width + ', Height: ' + height);
